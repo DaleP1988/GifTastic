e06a7368979c673813f3243721ddb6dcbd8289b7
Garnet <garnet.dianthus@gmail.com>
2018-07-28 18:22:58 -0700

    edits to all files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,182 +1,103 @@
 
 
-//one tree hill
-//the oc
-//felicity
-//party of five
-//freaks and geeks
-//buffy the vampire slayer
-//7th Heaven
-//Roswell
-//My So Called Life
-//
 
-      
-var topics = ["Gilmore Girls", "One Tree Hill", "The O.C."];
+$( document ).ready(function() {
 
-// Constructing a URL to search Giphy for the name of the person who said the quote
-var queryURLPrefix = "https://api.giphy.com/v1/gifs/search?q=" ;
-var queryURLSuffix =  "&api_key=hCIPnDDgr1lypssStOzyyS7SUV5QHnkI" + "&limit=10" ;
-var topicData = [];
-
-//create these to build
-
-$("#addShow").click(function(){
-  onAddShowClicked();
-});
-for (var i = 0; i < topics.length; i++) { 
-  createNewTopicButton(topics[i]); //review parameters  
- 
-}
-
-function createNewTopicButton(topicName) {
-  var topic = {
-    name: topicName,
-    images: []
-  };
-  topicData.push(topic);
-  var i = topicData.length-1;
-  fetchImages(topic, i);
-  var button = $("<button>").text(topic.name).data("index", i);
-  button.addClass("topicButton");
-  $("#buttons").append(button);
-  newImages(button, topic, i);
-}
-
-function fetchImages(topic, index) {    //this is a new variable being created
-  var topicParam = topic.name.toLowerCase().split(" ").join("+"); //standard for API queries
-  var queryURL = queryURLPrefix + topicParam + queryURLSuffix;
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-  }).then(function(response) {
-    // Storing an array of results in the results variable. //SET TO VAR images, only need to declare once
-    var pics;
-    
-    pics = response.data;
-      
-    // Looping over every result item.
-    //could have made local variable with new name for topic.name. this is REFACTORING <<<<
-    //webstorm has more editor options. good tool to use. 
-    for (var i = 0; i < pics.length; i++) {
-
-      var topicName = topic.name;   //let and var are mostly interchangeable. let is newer. const means does not change/fixed; if you set to 0 it will stay 0.
-      var imageData = {
-        stillImage: pics[i].images.original_still.url,
-        animatedImage: pics[i].images.original.url,
-        rating: pics[i].rating,
-        topicName: topicName
-      };
-      topic.images.push(imageData);
-
-    }
+  // TV Show Array. Will also house user input shows later.
   
-  });
-}  
-
-function newImages(button, topic, index){
-  button.click(function(){  // needed to change this to class so these can be clicked individually
-    $("#gifs").html("");
-    topic.images.sort(function(image1, image2){     //can switch around if it sorts backwards
-      return image2.rating.localeCompare(image1.rating);   //locale compare will use string comparison rules based on the current location language (and all punc.)
-    });                                                   // <<<<<< play with the sort function. gives a function and gives two items at a time and determines whether to swap. 
-    for (var i = 0; i < topic.images.length; i++) { //can do multiple "dots" for complex objects
-
-      var showDiv = $("<div>");
-      var p = $("<p>").text("Rating:" + topic.images[i].rating);
-      var showImage = $("<img>");
-      showImage.attr("src", topic.images[i].stillImage);
-      showDiv.append(p);
-      showDiv.append(showImage).addClass("topicImage");
-      $("#gifs").prepend(showDiv);
-      showImage.attr("currentImage", topic.images[i].stillImage); //be clear on contents of variable.
-      animateButton(showImage, topic, i);
-
-    }
-   
-  });
-}
-    //NEXT FUNCTION: animate images when clicked; see Pausing Gifs Activity
-// with classes you can search all at once and do a function to all at once. id only works for one.
-// with an id, you would have to use a unique id for each one.
-function animateButton(image, topic, index){
-  image.on("click",function(){
-      var imageData = topic.images[index];
-      var state = $(this).attr("currentImage");
-      if (state === imageData.stillImage) {
-        $(this).attr("src", imageData.animatedImage);
-        $(this).attr("currentImage", imageData.animatedImage);
-      } else {
-        $(this).attr("src", imageData.stillImage); //review this on my own
-        $(this).attr("currentImage", imageData.stillImage);
+  
+  var shows = ["One Tree Hill", "The O.C.", "My So-Called Life", "Gilmore Girls", "Charmed", "Felicity", "Buffy the Vampire Slayer", "7th Heaven", "Gossip Girl", "Dawson's Creek","Friends", "Freaks and Geeks", "90210"];
+  // ***********************************Functions & Methods***********************************************
+  
+  
+  // FOR Buttons ***********************************************************************************************
+  
+  
+  function presentGifButtons(){
+      $("#gifButtonsDisplay").empty(); // CLEAR DIV, avoid duplicate results******************************************************
+      for (var i = 0; i < shows.length; i++){
+          var gifButton = $("<button>");
+          gifButton.addClass("show");
+          gifButton.addClass("btn btn-primary");
+          gifButton.attr("data-name", shows[i]);
+          gifButton.text(shows[i]);
+          $("#gifButtonsDisplay").append(gifButton);
+      }
+  }
+  // FOR ADD Show button **************************************************************************************************
+  function addShowButton(){
+      $("#addShow").on("click", function(){
+      var show = $("#show-input").val().trim();
+      if (show == ""){
+        return false; // avoid making a blank button ******************************************
+      }
+      shows.push(show);
+  
+      presentGifButtons();
+      return false;
+      });
+  }
+  // FOR REMOVE show button ***************************************************************************************************
+      
+  function removeShowButton(){
+      $("removeShow").on("click", function(){
+      shows.pop(show);
+      presentGifButtons();
+      return false;
+      });
+  }
+  // FOR DISPLAY shows ************************************************************************************************************
+  function showAllGifs(){
+      var show = $(this).attr("data-name");
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
+      console.log(queryURL); // URL checkpoint
+      $.ajax({
+          url: queryURL,
+          method: 'GET'
+      })
+      .done(function(response) {
+          console.log(response); // data checkpoint
+          $("#showView").empty(); // erase div content for new. 
+          var results = response.data; //show results of gifs
+          if (results == ""){
+            alert("no gif for this one");
+          }
+          for (var i=0; i<results.length; i++){
+  
+              var gifDiv = $("<div>"); //make div for the gifs
+              gifDiv.addClass("gifDiv");
+              // access rating
+              var gifRating = $("<p>").text("Rating: " + results[i].rating);
+              gifDiv.append(gifRating);
+              // connect to gif
+              var gifImage = $("<img>");
+              gifImage.attr("src", results[i].images.fixed_height_small_still.url); // still image stored at src
+              gifImage.attr("data-still",results[i].images.fixed_height_small_still.url); // still image at data-still
+              gifImage.attr("data-animate",results[i].images.fixed_height_small.url); // animated image at data-animate
+              gifImage.attr("data-state", "still"); // set image state
+              gifImage.addClass("image");
+              gifDiv.append(gifImage);
+              // pulling still image of gif
+              // adding div of gifs to gifsView div
+              $("#showView").prepend(gifDiv);
+          }
+      });
+  }
+  // CALLING THE FUNCTIONS******************************************************************************************************
+  presentGifButtons(); // displays list of actions already created
+  addShowButton();
+  removeShowButton();
+  
+  // CHANGING IMAGE STATE with .on click function ************************
+  $(document).on("click", ".show", showAllGifs);
+  $(document).on("click", ".image", function(){
+      var state = $(this).attr('data-state');
+      if ( state == 'still'){
+          $(this).attr('src', $(this).data('animate'));
+          $(this).attr('data-state', 'animate');
+      }else{
+          $(this).attr('src', $(this).data('still'));
+          $(this).attr('data-state', 'still');
       }
   });
-}
-
-function onAddShowClicked(){
-  var userChoiceInput = $("#show-input").val();
-  createNewTopicButton(userChoiceInput);
-
-}
-
-
-//DEBUGGING
-//index should be the new number
-//append should add the new button to the div - ask the div for children, 
-//check the elements tab of the debugger
-
-    
-
-
-
-
-
-
-
-
-  //NEXT FUNCTION: check these class activities: 6.3.13 button triggered AJAX, 6.3.15 dynamic elements 
-      //at the on click to get the index, 
-      //look up topic data at the index
-      //iterate over images and create image elements on the page, 
-      //add on click for each image 
-      //all of this is in the same function
-      // images will go the gifs id
-
-
-       //FUNCTION:
-        //take user input
-        //append to array
-        //make buttons 
-        //new button functionality -based on the loop
-
-      //NEXT FUNCTION: start the gif 6.3.15 -
-
-// NEXT FUNCTION:
-//take user input
-//append to array
-//make buttons 
-//new button functionality -based on the loop
-
-
-
-              // Only taking action if the photo has an appropriate rating
-              // if (topics[i].rating !== "r" && topics[i].rating !== "pg-13") {
-                // Creating a div with the class "item"
-        //         var gifDiv = $("<div class='item'>");
-
-        //         // Storing the result item's rating
-        //         var rating = topics[i].rating;
-
-        //         // Creating a paragraph tag with the result item's rating
-        //         var p = $("<p>").text("Rating: " + rating);
-
-
-
-    
-
-      
-
-    //
-   
-        
-     
+  });
